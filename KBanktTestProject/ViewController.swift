b23ad60e6a8b592078f1cbba8f4ef0c133073702
Tarlan Hekimzade <qochaq@gmail.com>
2020-10-31 20:07:12 +0400

    Initial Commit

---
+++ KBanktTestProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KBanktTestProject
+//
+//  Created by Tarlan Hekimzade on 31.10.2020.
+//  Copyright © 2020 Tarlan Hekimzade. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
